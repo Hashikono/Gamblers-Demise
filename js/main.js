4391150e0ef7ba3c468eddc5c0f9e1d3b37c5c93
hashikono <hashikono@outlook.com>
2026-05-09 14:36:47 -0700

    Finished initial planning for the gameplay algorithm

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Global parameters
+var dayCounter = 0;
+
+/* 
+ANIMATION:
+So when the day begins, the bunnies that will come and the amount 
+that they will earn is already established
+
+the animation at that point on the screen is basically a random
+animation that will pop up, and over the span of 30 seconds, the 
+profits calculated and the negative stuff 
+
+
+To do:
+- bunnies are objects
+- make the parameters for each slot machine
+- make the slot calculation function
+- bunnies profits -> bunnies.setGain(slot(2))...
+    - bunny tries slot 2
+
+Pitch:
+Slot machine tycoon
+but tycoon games are too boring, aren't you just staring at
+the screen all day...
+that's what our owners thought, but little did they know
+the spirit of the rabbit who's people you scammed is seeking its
+revenge...
+spirit of the white rbbit at the end of the day makes 
+you play the very slot machines you created
+
+the basic goal is to survive as long as possible
+
+
+*/
+
+const array = new Uint32Array(1);
+window.crypto.getRandomValues(array);
+console.log(array[0]); 
+
+
+
+
+
+
+
