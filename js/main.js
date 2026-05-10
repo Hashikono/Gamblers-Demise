e4488458bc7c76285371829ca9fc5bf58901cc40
hashikono <hashikono@outlook.com>
2026-05-10 10:51:36 -0700

    Currently trying to get the current bunny's mney to be considered but I think I'm confusing everything

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Global parameters
 var dayCounter = 0;
+var officialBunnyTurnout = 0;
 
 //Amount of money to start
 var playerMoney = 1000;
@@ -12,9 +13,24 @@ var bunnyTries = 3;
 var whiteRabbitChance = 100;
 //Max amount of times the user has to gamble in phase 2
 var maxAmountGamble = 4;
+//Percentage of total gain/loss from bunnies
+var percentageGain = 100;
 
-//Bunny class
-class Bunny {
+//stores machines
+var machineCount = 9; //Must hard code this
+var zones = [true,false,false]; //zones available
+var machineList = []; //will be filled in the next section
+//initialize available machines
+for (let i = 0; i < machineCount; i++){
+    machineList.push(new Machine(i+1, false));
+}
+for (let i = 0; i < machineCount/zones.length; i++){
+    machineList[i].setMachineAvail(true);
+}
+
+//Bunny class : 
+//money() | trial()
+class Bunny { 
     constructor() {
         // (int) money
         this.money = bunnyMoney;
@@ -27,16 +43,21 @@ class Bunny {
     setMoney(amount) {this.money = amount;}
     getMoney() {return this.money;}
 
-    //Handles Trials
+    //Handles Trials (deprecated)
     modTrial(amount) {this.money = this.money + amount;}
     setTrial(amount) {this.money = amount;}
     getTrial() {return this.money;}
     
 }
 
-//Machine [zone] class
+//Machine [zone] class : 
+//machineNum() | machineAvail() | winRate() | payout() | minBet() | override()
 class Machine {
-    constructor() {
+    constructor(num, availability) {
+        // Machine number
+        this.machineNum = num;
+        //Machine available/unlocked
+        this.machineAvail = availability;
         // Win rate : 0-100% (0% weight)
         this.winRate = 50;
         // Payout : 1x, 1.5x, 2x, 2.5x, 3x (10% per tier) (50% weight)
@@ -46,6 +67,14 @@ class Machine {
         // Override win rate : true/false (0% weight)
         this.override = false;
     }
+
+    //Handles machine number
+    setMachineNum(limit) {this.machineNum = limit;}
+    getMachineNum() {return this.machineNum;}
+
+    //Handles machine availability
+    setMachineAvail(limit) {this.machineAvail = limit;}
+    getMachineAvail() {return this.machineAvail;}
 
     //Handles win rate
     setWinRate(limit) {this.winRate = limit;}
@@ -73,60 +102,84 @@ function rand100() {
 
 //Main machine calculation function
 function machineCalc(mach){
-    //mach = machine object
-    //percentage chance of bunnies to come
-    let attraction = 0;
-    //Amount of bunnies that actually show up
-    let bunnyTurnout = 0;
-    //list of winning bunnies
-    let bunnyList = [];
+    //if machine is even unlocked
+    if (mach.getMachineAvail()){
+        //mach = machine object
+        //percentage chance of bunnies to come
+        let attraction = 0;
+        //Amount of bunnies that actually show up
+        let bunnyTurnout = 0;
+        //list of bunny wins (int)
+        let bunnyList = [];
 
-    //payout weight consideration
-    const pay = parseFloat(mach.getPayout());
-    if (payout >= 3.0) attraction += 50;
-    else if (payout >= 2.5) attraction += 40;
-    else if (payout >= 2.0) attraction += 30;
-    else if (payout >= 1.5) attraction += 20;
-    else if (payout >= 1.0) attraction += 10;
+        //payout weight consideration
+        const pay = parseFloat(mach.getPayout());
+        if (payout >= 3.0) attraction += 50;
+        else if (payout >= 2.5) attraction += 40;
+        else if (payout >= 2.0) attraction += 30;
+        else if (payout >= 1.5) attraction += 20;
+        else if (payout >= 1.0) attraction += 10;
 
-    //bet weight consideration
-    attraction += Math.round(mach.getMinBet()/2);
+        //bet weight consideration
+        attraction += Math.round(mach.getMinBet()/2);
 
-    //setting bunny turnout
-    for (let i = 0; i < maxBunnies; i++){
-        let tempRandom = rand100();
-        if (tempRandom <= attraction){
-            bunnyTurnout++;
+        //setting bunny turnout
+        for (let i = 0; i < maxBunnies; i++){
+            let tempRandom = rand100();
+            if (tempRandom <= attraction){
+                bunnyTurnout++;
+            }
         }
-    }
+        
+        //setting official turnout
+        officialBunnyTurnout += bunnyTurnout;
 
-    //official winnings calculation
-    for (let i = 0; i < bunnyTurnout; i++){
-        //individual bunny earnings
-        let earnings = 0;
-        //3 trials for each bunny
-        for (let t = 0; t < 3; t++){
-            
+        //official winnings calculation
+        for (let i = 0; i < bunnyTurnout; i++){
+            //individual bunny earnings
+            let earnings = 0;
+            let currentBunny = new Bunny();
+
+            function earningCalcShort(){
+                return mach.getMinBet()*mach.getPayout();
+            }
+
+            //trials for each bunny
+            for (let t = 0; t < bunnyTries; t++){
+                //if they still have money to bet on the machine
+                if (currentBunny.getMoney() >= mach.getMinBet()){
+                    //If they win
+                    let winNum = rand100();
+                    currentBunny.modMoney(-mach.getMinBet());
+                    earings += mach.getMinBet();
+
+                    if (mach.getOverride() && winNum <= 10){ //overriding
+                            earnings += currentBunny.getMoney();
+                            currentBunny.setMoney(0);
+                        } else {
+                            earnings += earningCalcShort;
+                            currentBunny.modMoney(-earningCalcShort);
+                        }
+                    } else if (winNum <= mach.getWinRate()){ //actual win
+                        earnings -= earningCalcShort;
+                        currentBunny.modMoney(earningCalcShort);
+                    } else { //actual loss
+                        if (currentBunny.getMoney() - earningCalcShort < 0){
+                            earnings += currentBunny.getMoney();
+                            currentBunny.setMoney(0);
+                        } else {
+                            earnings += earningCalcShort;
+                            currentBunny.modMoney(-earningCalcShort);
+                        }
+                    }
+                }
+            }
+
+            bunnyList.push(earnings*(percentageGain/100));
         }
 
-        bunnyList.push(earnings);
+        return bunnyList;    
     }
-
-    return bunnyList;
-
-
-    /*
-        // Win rate : 0-100% (0% weight)
-        this.winRate = 50;
-        // Payout : 1x, 1.5x, 2x, 2.5x, 3x (10% per tier) (50% weight)
-        this.payout = 2.0;
-        // Minimum bet : $0 - $100 (round[dollar/2] => %) (50% weight)
-        this.minBet = 50;
-        // Override win rate : true/false (0% weight)
-        this.override = false;
-    */
-    
-
 }
 
 
